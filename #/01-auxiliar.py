e31627a597d101768e853b4f8e1391103718ef82
joaoedhansen <joaoedhansen@gmail.com>
2025-01-20 14:58:14 -0300

    a
    a

--- #/01-auxiliar.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import time
-
-# time.sleep(5) #pausa de 5 segundos
-# print(pyautogui.position()) #informar a posição do mouse na tela
-
-import os
-print("Diretório atual:", os.getcwd())
---
+++ PYTHON INTERMEDIÁRIO/23-funcao_zip_lista.py
@@ -0,0 +1,7 @@
+estados = ['Rio Grande do Sul', 'São Paulo', 'Bahia', 'Rio de Janeiro']
+estados_siglas = ['RS', 'SP', 'BA', 'RJ']
+
+lista_combinada = zip(estados, estados_siglas)
+
+for item in lista_combinada:
+    print(item)
